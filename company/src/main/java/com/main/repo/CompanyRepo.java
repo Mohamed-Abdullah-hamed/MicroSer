3ee24868dddf5779988125fa6440d6776329ab3f
Mohamed-Abdullah-hamed <105975532+Mohamed-Abdullah-hamed@users.noreply.github.com>
2024-11-06 00:31:57 +0200

    Delete company directory

--- company/src/main/java/com/main/repo/CompanyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.main.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.main.entity.Company;
-
-public interface CompanyRepo extends JpaRepository<Company,Integer>{
-
-}
